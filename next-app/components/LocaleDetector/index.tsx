5d0d2222cec7f10900ebd1fd93d312401c609bc5
hohlovkiril <hohlov.kiril.ua@gmail.com>
2025-12-22 11:18:01 +0200

    Update

--- next-app/components/LocaleDetector/index.tsx
+++ next-app/components/LocaleDetector/index.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { Locale } from "@/libs/types";
-import i18n from "@/libs/i18n/i18n.client";
 import { useEffect } from "react";
+import i18n from "@/libs/i18n/i18n.client";
+import { Locale } from "@/libs/types";
 
 export type LocaleDetectorProps = {
   locale: Locale;
@@ -13,10 +13,18 @@ export default function LocaleDetector({
 }: LocaleDetectorProps) {
   
   useEffect(() => {
-    if (typeof window !== 'undefined' && i18n.language !== locale) {
-      i18n.changeLanguage(locale);
+    if (!locale) return;
+
+    const changeLanguage = async () => {
+      if (i18n.language !== locale) {
+        i18n.changeLanguage(locale)
+      }
     }
+
+    changeLanguage();
   }, [locale])
 
   return null;
+
+  return `Server locale: ${locale} | Client locale: ${i18n.language}`;
 }
